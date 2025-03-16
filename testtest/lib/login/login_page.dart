98bfc7ff918fa9aeb7e6d2c414a2b637876fba5c
Tiago92869 <tiagoamartins02@gmail.com>
2025-03-16 17:13:07 +0100

    asdads adsasda adasda

---
+++ testtest/lib/login/login_page.dart
@@ -0,0 +1,204 @@
+// login_page.dart
+import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
+import 'create_account_page.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 400,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/background1.png'),
+                  fit: BoxFit.fill,
+                ),
+              ),
+              child: Stack(
+                children: <Widget>[
+                  Positioned(
+                    left: 30,
+                    width: 80,
+                    height: 200,
+                    child: FadeInUp(
+                      duration: Duration(seconds: 1),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/images/starfish1.png'),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 140,
+                    width: 80,
+                    height: 150,
+                    child: FadeInUp(
+                      duration: Duration(milliseconds: 1200),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/images/starfish2.png'),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    child: FadeInUp(
+                      duration: Duration(milliseconds: 1600),
+                      child: Container(
+                        margin: EdgeInsets.only(top: 50),
+                        child: Center(
+                          child: Text(
+                            "Login",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(30.0),
+              child: Column(
+                children: <Widget>[
+                  FadeInUp(
+                    duration: Duration(milliseconds: 1800),
+                    child: Container(
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: Color(0xFF557BE9)),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color(0x7D9AEE),
+                            blurRadius: 20.0,
+                            offset: Offset(0, 10),
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.all(8.0),
+                            decoration: BoxDecoration(
+                              border: Border(
+                                bottom: BorderSide(color: Color(0xFF557BE9)),
+                              ),
+                            ),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "Email or Phone number",
+                                hintStyle: TextStyle(color: Colors.grey[700]),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.all(8.0),
+                            child: TextField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "Password",
+                                hintStyle: TextStyle(color: Colors.grey[700]),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  FadeInUp(
+                    duration: Duration(milliseconds: 1900),
+                    child: GestureDetector(
+                      child: Container(
+                        height: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          gradient: LinearGradient(
+                            colors: [Color(0xFF557BE9), Color(0xFF7D9AEE)],
+                          ),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Login",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  FadeInUp(
+                    duration: Duration(milliseconds: 1950),
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => CreateAccountPage(),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        height: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          gradient: LinearGradient(
+                            colors: [
+                              Color.fromRGBO(85, 123, 233, 1),
+                              Color.fromRGBO(125, 154, 238, 1),
+                            ],
+                          ),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Create Account",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 70),
+                  FadeInUp(
+                    duration: Duration(milliseconds: 2000),
+                    child: Text(
+                      "Forgot Password?",
+                      style: TextStyle(color: Color(0xFF557BE9)),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ transition_animation/lib/main.dart
@@ -0,0 +1,201 @@
+import 'Dashboard.dart';
+import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+
+void main() =>
+    runApp(MaterialApp(debugShowCheckedModeBanner: false, home: HomePage()));
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  PageController? _pageController;
+
+  AnimationController? rippleController;
+  AnimationController? scaleController;
+
+  Animation<double>? rippleAnimation;
+  Animation<double>? scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _pageController = PageController(initialPage: 0);
+
+    rippleController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+
+    scaleController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    )..addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        Navigator.push(
+          context,
+          PageTransition(type: PageTransitionType.fade, child: Dashboard()),
+        );
+      }
+    });
+
+    rippleAnimation = Tween<double>(
+      begin: 80.0,
+      end: 90.0,
+    ).animate(rippleController!)..addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        rippleController?.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        rippleController?.forward();
+      }
+    });
+
+    scaleAnimation = Tween<double>(
+      begin: 1.0,
+      end: 30.0,
+    ).animate(scaleController!);
+
+    rippleController?.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        children: <Widget>[
+          makePage(image: 'assets/images/two.jpg'),
+          makePage(image: 'assets/images/one.jpg'),
+          makePage(image: 'assets/images/three.jpg'),
+        ],
+      ),
+    );
+  }
+
+  Widget makePage({image}) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(image: AssetImage(image), fit: BoxFit.cover),
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomRight,
+            colors: [
+              Colors.black.withOpacity(.3),
+              Colors.black.withOpacity(.2),
+            ],
+          ),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 40, horizontal: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 60),
+                  Text(
+                    'Good day\nhealth body',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 200, 224, 229),
+                      fontSize: 45,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'With this app you can try different activities\nand choose what is more enjoyable for you.',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 185, 202, 206),
+                      fontSize: 18,
+                      height: 1.4,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                  SizedBox(height: 40),
+                ],
+              ),
+              // Column(
+              //   crossAxisAlignment: CrossAxisAlignment.start,
+              //   children: <Widget>[
+              //     FadeAnimation(1, Text("15", style: TextStyle(color: Colors.yellow[400], fontSize: 40, fontWeight: FontWeight.bold),)),
+              //     FadeAnimation(1.2, Text("Minutes", style: TextStyle(color: Colors.white, fontSize: 30),)),
+              //   ],
+              // ),
+              // SizedBox(height: 30,),
+              // Column(
+              //   crossAxisAlignment: CrossAxisAlignment.start,
+              //   children: <Widget>[
+              //     FadeAnimation(1, Text("3", style: TextStyle(color: Colors.yellow[400], fontSize: 40, fontWeight: FontWeight.bold),)),
+              //     FadeAnimation(1.2, Text("Exercises", style: TextStyle(color: Colors.white, fontSize: 30),)),
+              //   ],
+              // ),
+              // SizedBox(height: 180,),
+              // FadeAnimation(1, Align(
+              //   child: Text("Start the morning with your health",
+              //   textAlign: TextAlign.center,
+              //   style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.w100),),
+              // )),
+              // SizedBox(height: 30,),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: AnimatedBuilder(
+                  animation: rippleAnimation!,
+                  builder:
+                      (context, child) => Container(
+                        width: rippleAnimation?.value,
+                        height: rippleAnimation?.value,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.white.withOpacity(.4),
+                          ),
+                          child: InkWell(
+                            onTap: () {
+                              scaleController?.forward();
+                            },
+                            child: AnimatedBuilder(
+                              animation: scaleAnimation!,
+                              builder:
+                                  (context, child) => Transform.scale(
+                                    scale: scaleAnimation?.value,
+                                    child: Container(
+                                      margin: EdgeInsets.all(10),
+                                      decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        color: Colors.white,
+                                      ),
+                                      child:
+                                          scaleController?.status ==
+                                                      AnimationStatus.forward ||
+                                                  scaleController?.status ==
+                                                      AnimationStatus.completed
+                                              ? null
+                                              : Center(
+                                                child: Icon(
+                                                  Icons.fingerprint,
+                                                  size: 40,
+                                                ),
+                                              ),
+                                    ),
+                                  ),
+                            ),
+                          ),
+                        ),
+                      ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
